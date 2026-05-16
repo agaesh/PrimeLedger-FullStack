8971e52997e754360b1f3f48668f50e52c466afe
agaes <agaes@AGAESH>
2026-05-16 18:22:45 +0800

    feat (db) - Configured database connection using connection string and created AppDbContext

---
+++ PrimeAPI/Infrasfructure/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeAPI.Infrasfructure
+{
+    using Microsoft.EntityFrameworkCore;
+    using PrimeAPI.Domain;
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
