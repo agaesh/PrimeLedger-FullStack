1d4d3079b37953386a062e682fb97d9b81a61db9
agaes <agaes@AGAESH>
2026-05-16 18:56:00 +0800

    feat(domain) - Add Product Metadata domain and relationships in AppDbContext

---
+++ PrimeAPI/Domain/ProductMetadata.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
+namespace PrimeAPI.Domain
+{
+
+    public enum Codetype { 
+        GROUP,
+        BRAND,
+        UOM
+    }
+
+    public enum StatusEnum
+    {
+        ACTIVE,
+        INACTIVE
+    }
+
+    public class ProductMetadata
+    {
+        public int Id { get; private set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        [Required]
+        public Codetype? type { get; set; }
+
+        [Required]
+        [Column("status")]
+        public StatusEnum? Status { get; set; }
+
+        // Scalar FK column
+        [Column("parent_id")]
+        public int? ParentId { get; set; }
+        public ProductMetadata? Parent { get; set; }   // navigation property
+
+        public ICollection<ProductMetadata> Children { get; set; } = new List<ProductMetadata>();
+
+
+        [Column("created_at")]
+        public DateTime createdAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime? updatedAt { get; set; }
+    }
+}
--- PrimeAPI/Infrasfructure/AppDbContext.cs
+++ PrimeAPI/Infrasfructure/AppDbContext.cs
@@ -8,5 +8,26 @@
             : base(options)
         {
         }
+        public DbSet<ProductMetadata> ProductMetadata { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ProductMetadata>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                // Tell EF Core to store the Enum as a string/text in the DB
+                entity.Property(e => e.Status)
+                      .HasConversion<string>()
+                      .HasMaxLength(50); // Good practice to limit the text column size
+
+                entity.HasOne(p => p.Parent)              // navigation to parent
+                     .WithMany(p => p.Children)          // navigation to children
+                     .HasForeignKey(p => p.ParentId)     // FK column
+                     .OnDelete(DeleteBehavior.Restrict); // prevent cascade delete
+            });
+        }
     }
 }
