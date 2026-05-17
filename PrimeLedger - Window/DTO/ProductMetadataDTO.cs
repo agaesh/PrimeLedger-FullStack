68b73a1ce56af3912a75b023aba84ff139e3bf8a
agaes <agaes@AGAESH>
2026-05-17 20:37:09 +0800

    chore - Adding DTO for ProductData
    Add ProductMetadataDTO class to DTO namespace
    
    Introduced an internal ProductMetadataDTO class in the PrimeLedger_Window.DTO namespace with properties for Id, Code, Description, Status, Type, create_at, and update_at to represent product metadata.

---
+++ PrimeLedger - Window/DTO/ProductMetadataDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace PrimeLedger_Window.DTO
+{
+    internal class ProductMetadataDTO
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+
+        public string create_at { get; set; }
+
+        public string update_at { get; set; }
+    }
+}
