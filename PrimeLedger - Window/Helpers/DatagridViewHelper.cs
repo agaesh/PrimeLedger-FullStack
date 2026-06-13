f2ebd1c274f572f6b66ca2baf34c02dd51b3d38f
agaes <agaes@AGAESH>
2026-06-13 10:46:52 +0800

    feat(DataGridViewHelper): add AddGridItem method to insert new items and refresh DataGridView

--- PrimeLedger - Window/Helpers/DatagridViewHelper.cs
+++ PrimeLedger - Window/Helpers/DatagridViewHelper.cs
@@ -41,5 +41,23 @@ namespace PrimeLedger.Helpers
                 }
             }
         }
+        /// <summary>
+        /// Adds a new item to a DataGridView bound via BindingSource/BindingList,
+        /// and refreshes the grid to show it.
+        /// </summary>
+        /// <typeparam name="T">Type of items in the BindingList.</typeparam>
+        /// <param name="dgv">Target DataGridView.</param>
+        /// <param name="newItem">Item to add.</param>
+        public static void AddGridItem<T>(DataGridView dgv, T newItem)
+        {
+            if (dgv.DataSource is BindingSource bs &&
+                bs.List is BindingList<T> list)
+            {
+                list.Add(newItem);
+
+                // Notify grid of changes
+                bs.ResetBindings(false);
+            }
+        }
     }
 }
