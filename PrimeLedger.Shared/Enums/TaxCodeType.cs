b28adcdb02cc0fa263a62cebc6afb66f8c205286
agaes <agaes@AGAESH>
2026-06-15 12:28:27 +0800

    chore - add taxcodetype enum class

---
+++ PrimeLedger.Shared/Enums/TaxCodeType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeLedger.Shared.Enums
+{
+    public enum TaxCodeType
+    {
+        GST,
+        SST,
+        VAT,
+        EXEMPT,
+        CUSTOMS
+    }
+}
