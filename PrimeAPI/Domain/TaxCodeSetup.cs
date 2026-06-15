0e0ff96cdabf6d9d0603f3dd8c1b938497664650
agaes <agaes@AGAESH>
2026-06-15 12:24:41 +0800

    feat - add taxcodesetup and taxcodehistory domain
    - taxcode setup for defining codes
    - taxcodesetup is for using the created tax code both purchase and sales

---
+++ PrimeAPI/Domain/TaxCodeSetup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using PrimeLedger.Shared.Enums;
+
+namespace PrimeAPI.Domain
+{
+    [Table("TaxCodeSetup")]
+    public class TaxCodeSetup
+    {
+        public int Id { get; set; } // PK
+        [Column("code")]
+        public string Code { get; set; } // GST6
+        [Column("description")]
+        public string Description { get; set; } // Goods and Services Tax
+        [Column("tax_rate")]
+        public decimal TaxRate { get; set; } // 6.00
+
+        [Column("tax_type")]
+        public TaxCodeType type { get; set; }
+        public ICollection<TaxCodeHistory> Histories { get; set; }
+    }
+}
