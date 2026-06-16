539c77beaf5b44b55093a273e9fb7b56bd6632fa
agaes <agaes@AGAESH>
2026-06-16 15:13:06 +0800

    feat: add GLAccount enums
    - Introduced AccountType enum to classify accounts as Asset, Expense, Equity, Liability, or Revenue
    - Added NormalBalance enum to define Debit and Credit orientation
    - Prepared enums for integration with GLAccount entity mapping

---
+++ PrimeLedger.Shared/Enums/AccountType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeLedger.Shared.Enums
+{
+    public enum AccountType
+    {
+        ASSET,
+        EXPENSE,
+        EQUITY,
+        LIABILTY,
+        REVENUE,
+    }
+}
