bb5807bed6e2ca09b93fc4956024fa0bd14b4e9a
agaes <agaes@AGAESH>
2026-06-16 22:55:58 +0800

    feat: add GlAccount migrations
    - Generated initial migration for GlAccount entity
    - Created table with columns for account details, enums, and hierarchy
    - Applied constraints and relationships for parent–child linkage

---
+++ PrimeAPI/Infrastructure/Migrations/20260616110747_add-gl-account-table-migration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrimeAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addglaccounttablemigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GlAccount",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    account_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    account_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    account_type = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    normal_balance = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    parent_acc_id = table.Column<int>(type: "int", nullable: true),
+                    is_allow_posting = table.Column<bool>(type: "bit", nullable: false),
+                    is_active = table.Column<bool>(type: "bit", nullable: false),
+                    created_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updated_date = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GlAccount", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GlAccount_GlAccount_parent_acc_id",
+                        column: x => x.parent_acc_id,
+                        principalTable: "GlAccount",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GlAccount_parent_acc_id",
+                table: "GlAccount",
+                column: "parent_acc_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GlAccount");
+        }
+    }
+}
