05c39c968e7bdaf9952dc8cb17ba172476959425
agaes <agaes@AGAESH>
2026-06-17 09:28:47 +0800

    feat - add gl account repository and its interface

---
+++ PrimeAPI/Application/Interface/IGLAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using PrimeAPI.Domain;
+using PrimeLedger.Shared.DTO.GLAccounts;
+using PrimeLedger.Shared.Enums;
+
+namespace PrimeAPI.Application.Interface
+{
+    public interface IGLAccountRepository
+    {
+        Task<IEnumerable<GlAccount>> GetAllAsync();
+        Task<GlAccount?> GetByIdAsync(int id);
+        Task<GlAccount?> GetByAccountCodeAsync(string accountCode);
+        Task<GlAccount> AddAsync(GlAccount account);
+        Task UpdateAsync(GlAccount account);
+        Task DeleteAsync(int id);
+
+        Task<IEnumerable<GlAccount>> GetByTypeAsync(AccountType type);
+    }
+}
