93e0e2c0bca69b3e245e83a0f1262a469e212206
agaes <agaes@AGAESH>
2026-06-17 10:04:24 +0800

    test(factory): add CustomWebApplicationFactory with InMemory DB configuration

---
+++ PrimeTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using PrimeAPI.Infrasfructure;
+using System.Linq;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(Microsoft.AspNetCore.Hosting.IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            // Tell application to use InMemory DB for testing
+            var dict = new Dictionary<string, string>
+            {
+                { "UseInMemory", "true" }
+            };
+            config.AddInMemoryCollection(dict);
+        });
+    }
+}
