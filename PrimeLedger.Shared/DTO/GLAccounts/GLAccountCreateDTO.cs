a6bf4aaea68d1b71a67dec01bee3b5268fe6d228
agaes <agaes@AGAESH>
2026-06-17 09:29:29 +0800

    feat - add GLAccount DTO for creating and updateing

---
+++ PrimeLedger.Shared/DTO/GLAccounts/GLAccountCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using PrimeLedger.Shared.Enums;
+
+namespace PrimeLedger.Shared.DTO.GLAccounts
+{
+    public class GlAccountCreateDTO
+    {
+        public string AccountCode { get; set; }
+        public string AccountName { get; set; }
+        public AccountType AccountType { get; set; }
+        public NormalBalance NormalBalance { get; set; }
+        public int? ParentAccountId { get; set; }
+        public bool AllowPosting { get; set; } = true;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ PrimeLedger.Shared/DTO/GLAccounts/GLAccountDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using PrimeLedger.Shared.Enums;
+namespace PrimeLedger.Shared.DTO.GLAccounts
+{
+    public class GlAccountDTO
+    {
+        public int Id { get; set; }
+        public string AccountCode { get; set; }
+        public string AccountName { get; set; }
+        public AccountType AccountType { get; set; }
+        public NormalBalance NormalBalance { get; set; }
+        public int? ParentAccountId { get; set; }
+        public bool AllowPosting { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+ }
