35153471ac721ea654ba13fa028f4b209e114571
agaes <agaes@AGAESH>
2026-06-17 09:36:38 +0800

    feat(service): implement GLAccountService with CRUD and query methods and its interface

---
+++ PrimeAPI/Application/Interface/IGLAccountService.cs
@@ -0,0 +1,16 @@
+﻿using PrimeLedger.Shared.DTO.GLAccounts;
+using PrimeLedger.Shared.Enums;
+
+namespace PrimeAPI.Application.Interface
+{
+    public interface IGLAccountService
+    {
+        Task<IEnumerable<GlAccountDTO>> GetAllAsync(int pageNumber = 1, int pageSize = 10);
+        Task<GlAccountDTO> GetByIdAsync(int id);
+        Task<GlAccountDTO> GetByAccountCodeAsync(string accountCode);
+        Task<int> AddAsync(GlAccountCreateDTO dto);
+        Task UpdateAsync(GlAccountStatusUpdateDTO dto);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<GlAccountDTO>> GetByTypeAsync(AccountType type, int pageNumber = 1, int pageSize = 10);
+    }
+}
