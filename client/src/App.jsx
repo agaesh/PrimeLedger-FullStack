4260af011c3af62e4e21612a0f570081518b5425
agaes <agaes@AGAESH>
2026-07-16 10:07:46 +0800

    feat(routing): register ChartsOfAccounts page in App.jsx
    - Added route entry for finance/accounts
    - Enables navigation to the new ChartsOfAccounts page
    - Scroll enabled to the page holding container instead of body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Sidebar from './Components/Sidebar/Sidebar'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from './Pages/Dashboard/Dashboards';
+import ChartsOfAccounts from './Pages/ChartsOfAccounts/index.jsx';
 
 function App() {
     return (
@@ -12,15 +13,15 @@ function App() {
                 <Sidebar />
 
                 {/* Main Content */}
-                <div style={{ flex: 1, padding: "20px" }}>
+                <div style={{ flex: 1, padding: "30px", overflowY:"scroll" }}>
                     <Routes>
                         <Route path="/" element={<Dashboard/>} />
                         <Route path="/invoice" element={<div>Create Invoice</div>} />
                         <Route path="/documents" element={<div>Documents</div>} />
                         <Route path="/directory" element={<div>Directory</div>} />
+                        <Route path="finance/accounts" element={<ChartsOfAccounts/>} /> 
                     </Routes>
                 </div>
-
             </div>
         </BrowserRouter>
     )
